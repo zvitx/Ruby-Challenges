ae4a6b9c46b382bba0e185d5fe14846755bb3094
Vitor Abreu <vitu.abreu09@gmail.com>
2025-02-20 13:33:55 -0300

    hash e operações funcionais

--- spaceage.rb
+++ spaceage.rb
@@ -2,25 +2,43 @@
 earthSecondsTime = 31557600.to_f #1 year in seconds in Earth
 
 puts "Choose a planet"
-puts "1. Mercury"
-puts "2. Venus"
-puts "3. Earth"
-puts "4. Mars"
-puts "5. Jupiter"
-puts "6. Saturn"
-puts "7. Uranus"
-puts "8. Neptune"
+puts "Mercury"
+puts "Venus"
+puts "Earth"
+puts "Mars"
+puts "Jupiter"
+puts "Saturn"
+puts "Uranus"
+puts "Neptune"
 
-choice = gets.chomp.to_i
+planet = gets.chomp
 
-puts "What's your age (in seconds)?"
-ageInSeconds = gets.chomp.to_f
+# puts "What's your age (in seconds)?"
+# ageInSeconds = gets.chomp.to_f
+ageInSeconds = 1000000000
+# puts ageInSeconds
+# ageInYears = 0.to_f
 
-puts ageInSeconds
-ageInYears = 0.to_f
+PlanetAges = {
+    "Mercury" => ageInSeconds / (earthSecondsTime * 0.2408467),
+    "Venus" => ageInSeconds / (earthSecondsTime * 0.61519726),
+    "Earth" => ageInSeconds / (earthSecondsTime),
+    "Mars" => ageInSeconds / (earthSecondsTime * 1.8808158),
+    "Jupiter" => ageInSeconds / (earthSecondsTime * 11.862615),
+    "Saturn" => ageInSeconds / (earthSecondsTime * 	29.447498),
+    "Uranus" => ageInSeconds / (earthSecondsTime * 84.016846),
+    "Neptune" => ageInSeconds / (earthSecondsTime * 164.79132)
+}
 
-case choice
-when 1
-    ageInYears = ageInSeconds / (earthSecondsTime * 0.2408467)
-    puts "Your Mercury age is #{ageInYears}"
+print PlanetAges.keys
+puts
+print PlanetAges.values
+puts "--------------------------"
+if PlanetAges.has_key?(planet)
+    puts "Ok"
+    print PlanetAges.keys
+elsif planet == "Pluto"
+    puts "Pluto is NOT a planet, try again."
+else
+    puts "Planet not found"
 end
